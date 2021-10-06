848a63c77875c1547a053f678bda7619cf21c505
RayenD <zufirka@icloud.com>
2021-10-06 07:44:29 +0300

    Initial Commit

---
+++ AppStoreJSONApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreJSONApi
+//
+//  Created by Rayen on 06.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
