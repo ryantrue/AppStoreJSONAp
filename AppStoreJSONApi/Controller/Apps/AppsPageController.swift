ea37c3068b43b1a58a229d9676d9fbdff661c6aa
RayenD <zufirka@icloud.com>
2021-10-12 21:06:13 +0300

    Passing Data Into Cells

--- AppStoreJSONApi/Controller/Apps/AppsPageController.swift
+++ AppStoreJSONApi/Controller/Apps/AppsPageController.swift
@@ -22,14 +22,18 @@ class AppsPageController: BaseListController, UICollectionViewDelegateFlowLayout
         fetchData()
     }
     
+    var editorsChoiceGames: AppGroup?
+    
     fileprivate func fetchData() {
-        print("Fetching new JSON DATA somehow...")
         Service.shared.fetchGames { (appGroup, err) in
             if let err = err {
                 print("Failed to fetch games:", err)
                 return
             }
-            print(appGroup?.feed.results)
+            self.editorsChoiceGames = appGroup
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
         }
     }
     
@@ -42,11 +46,15 @@ class AppsPageController: BaseListController, UICollectionViewDelegateFlowLayout
         return .init(width: view.frame.width, height: 300)
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId , for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId , for: indexPath) as! AppsGroupCell
+        
+        cell.titleLabel.text = editorsChoiceGames?.feed.title
+        cell.horizontalController.appGroup = editorsChoiceGames
+        cell.horizontalController.collectionView.reloadData()
         return cell
     }
     
--- AppStoreJSONApi/Service/Service.swift
+++ AppStoreJSONApi/Service/Service.swift
@@ -39,7 +39,11 @@ class Service {
         }.resume()
     }
     func fetchGames(completion: @escaping (AppGroup?, Error?) -> ()) {
-        guard let url = URL(string: "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/50/explicit.json") else { return }
+        
+//        "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/50/explicit.json"
+        
+        let urlString = "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-grossing/all/50/explicit.json"
+        guard let url = URL(string: urlString) else { return }
         
         URLSession.shared.dataTask(with: url) { (data, resp, err) in
             
